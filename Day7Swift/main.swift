bdcac7bcca87adf34833e5ce9bde4f4518a2a56b
Emil <emilabrahamz@gmail.com>
2019-02-19 18:38:20 -0500

    Initial Commit

---
+++ Day7Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day7Swift
+//
+//  Created by Akhil James Joseph on 2019-02-19.
+//  Copyright © 2019 Emil Abraham. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
